3f9cf75bc02e3c932f5fd227c34ee55070ee7a7f
codepapa360 <codepapa360@gmail.com>
2023-08-29 18:10:30 +0600

    added-core-functionality

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import darkMode from "./darkMode";
 
 function init() {
+  const inputPx = document.getElementById("pixel");
+  const inputRem = document.getElementById("rem");
+
+  function calcPixel() {
+    const px = inputPx.value;
+    const output = +px / 16;
+
+    inputRem.value = output;
+  }
+
+  function calcRem() {
+    const rem = inputRem.value;
+    const output = +rem * 16;
+    inputPx.value = output;
+  }
+
   // Start dark mode functionality
   darkMode();
+
+  inputPx.addEventListener("input", calcPixel);
+  inputRem.addEventListener("input", calcRem);
 }
 
 document.addEventListener("DOMContentLoaded", init);
